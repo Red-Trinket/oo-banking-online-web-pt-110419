af933e9006759fd48b4e884877e8a1ef5da7696b
Matthew Jones <matts3371@gmail.com>
2019-12-15 22:30:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,6 @@ class Transfer
   end
 
   def execute_transaction
-    if @receiver.status = 'open'
       if @sender.balance > @amount && @status == "pending"
         @sender.balance -= @amount
         @receiver.balance += @amount
@@ -22,10 +21,6 @@ class Transfer
         @status = "rejected"
         return "Transaction rejected. Please check your account balance."
       end
-    else 
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    end
   end
 
   def reverse_transfer
