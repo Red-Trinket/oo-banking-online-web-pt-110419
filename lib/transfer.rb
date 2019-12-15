36b4041d67de6352dbbda2d45116cfa69339dd11
Matthew Jones <matts3371@gmail.com>
2019-12-15 22:38:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
         return "Transaction rejected. Please check your account balance."
       end
     else 
-      return "rejected"
+      return "Transaction rejected. Please check your account balance."
     end 
   end
 
